09facd150d96752fff80ea122113d57edeab19cd
AlienXandr <anshostak@bk.ru>
2022-01-07 18:19:15 +0300

    Добавлены модальные окна, кнопка вверх, отправка email и локальный сервер  пропущены

--- src/js/script.js
+++ src/js/script.js
@@ -60,4 +60,35 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Модальные окна
+    $('[data-modal=consultation]').on('click', function (){
+        $('.overlay, #consultation').fadeIn('slow')
+    });
+    $('.modal__close').on('click', function (){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+    })
+    // $('.button_mini').on('click', function (){
+    //     $('.overlay, #order').fadeIn('slow')
+    // })
+    $('.button_mini').each(function (i){
+        $(this).on('click', function (){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow')
+        });
+    });
+
+    // Плавный скролл и кнопка вверх
+    $(window).scroll(function (){
+        if ($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function (){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    })
 });
